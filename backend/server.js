63a1f5c89c2c8c64fb8b5265d5b78ade35210d3d
Oscar Ko <OscarKoEmail@gmail.com>
2019-11-11 10:44:03 -0500

    Create route files and connect to server.js

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,10 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// REQUIRE ROUTE FILES =========================
+const productRoutes = require("./routes/products.js");
+const orderRoutes = require("./routes/orders.js");
+
 // APP USE =====================================
 app.use(cors());
 app.use(express.json());
@@ -22,6 +26,10 @@ mongoose.connect(url, {
 mongoose.connection.once('open', () => {
     console.log('MongoDB database connection established!');
 });
+
+// ROUTERS ==========================================
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
 
 // LISTENER =========================================
 app.listen(port, () => {
