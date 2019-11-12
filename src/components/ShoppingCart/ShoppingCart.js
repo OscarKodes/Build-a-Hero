ad6154a201b4d5885d14eef086532f93b84e7a36
Oscar Ko <OscarKoEmail@gmail.com>
2019-11-12 16:24:48 -0500

    Add 'add' buttons to product cards, and push products to shopping cart

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -1,10 +1,28 @@
 import React from "react";
 
-const shoppingCart = () => (
-    <div>
-        This is shoppingCart.
-    </div>
-)
+const shoppingCart = (props) => {
+
+    let allCart = null;
+
+    if (props.cart.length > 0) {
+        allCart = props.cart.map(product => {
+            return (
+                <div key={product._id}>
+                    <p>
+                        {product.title}
+                    </p>
+                </div>
+            )
+        })
+    }
+
+    return (
+        <div>
+            <h3>Shopping Cart</h3>
+            {allCart}
+        </div>
+    )
+}
 
 export default shoppingCart;
 
